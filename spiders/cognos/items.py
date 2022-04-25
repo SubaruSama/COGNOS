bacf47b8fd2057745b170c324bb4c9a038b4db4e
SubaruSama <andresfauque@gmail.com>
2022-04-24 22:39:41 -0300

    Construindo o primeiro bot, tendo dificuldades mas ate agora normal

--- spiders/cognos/items.py
+++ spiders/cognos/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class CisaBulletinsItem(scrapy.Item):
+class CognosItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
--- spiders/cognos/settings.py
+++ spiders/cognos/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for cisa_bulletins project
+# Scrapy settings for cognos project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'cisa_bulletins'
+BOT_NAME = 'cognos'
 
-SPIDER_MODULES = ['cisa_bulletins.spiders']
-NEWSPIDER_MODULE = 'cisa_bulletins.spiders'
+SPIDER_MODULES = ['cognos.spiders']
+NEWSPIDER_MODULE = 'cognos.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'cisa_bulletins (+http://www.yourdomain.com)'
+#USER_AGENT = 'cognos (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'cisa_bulletins.middlewares.CisaBulletinsSpiderMiddleware': 543,
+#    'cognos.middlewares.CognosSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'cisa_bulletins.middlewares.CisaBulletinsDownloaderMiddleware': 543,
+#    'cognos.middlewares.CognosDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'cisa_bulletins.pipelines.CisaBulletinsPipeline': 300,
+#    'cognos.pipelines.CognosPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -86,3 +86,17 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Custom
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+    'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,
+}
+
+FAKEUSERAGENT_PROVIDERS = [
+    'scrapy_fake_useragent.providers.FakeUserAgentProvider',
+    'scrapy_fake_useragent.providers.FakerProvider',  
+    'scrapy_fake_useragent.providers.FixedUserAgentProvider',
+]
