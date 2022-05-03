691b0e7d0ec999d2b26e27d5b9533b8d31b51345
SubaruSama <andresfauque@gmail.com>
2022-05-03 13:26:44 -0300

    Utilizando o item correto e configurando o input_processor para remover as tags de HTML

--- spiders/cognos/items.py
+++ spiders/cognos/items.py
@@ -3,10 +3,19 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_comments, remove_entities, remove_tags
 
 
-class CognosItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CognosItem(Item):
+    username = Field(input_processor=MapCompose(remove_tags))
+    posts_quantity = Field(input_processor=MapCompose(remove_tags))
+    date_joined = Field(input_processor=MapCompose(remove_tags))
+    reputation = Field(input_processor=MapCompose(remove_tags))
+    info_date_post = Field(input_processor=MapCompose(remove_tags))
+    post_content = Field(input_processor=MapCompose(remove_tags))
+    is_admin = Field()
+    is_special_user = Field()
+    title = Field(input_processor=MapCompose(remove_tags))
