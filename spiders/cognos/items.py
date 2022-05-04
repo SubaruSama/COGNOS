3903851505dfe44e2cc32fa5422e8c2578f73bdc
SubaruSama <andresfauque@gmail.com>
2022-05-04 02:13:39 -0300

    Adicionado mais uns xpath interessantes no arquivo paths_css_xpath.txt
    A minha ideia anterior de salvar o "nível" de cada usuário (normal user, special user e admin) não deu certo. Irei precisar remodelar o item e mudar algumas coisas. TODO
    
    Agora no item['usernam'] ta salvando todos os usuários sem distinção. Para poder fazer isso tive que passar toda a string do xpath (talvez pq estou usando o relativo, como //*[@id="foo"]? talvez, não li quase nada de xpath)

--- spiders/cognos/items.py
+++ spiders/cognos/items.py
@@ -5,7 +5,7 @@
 
 from scrapy import Item, Field
 from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose
+from itemloaders.processors import MapCompose
 from w3lib.html import remove_comments, remove_entities, remove_tags
 
 
@@ -16,6 +16,6 @@ class CognosItem(Item):
     reputation = Field(input_processor=MapCompose(remove_tags))
     info_date_post = Field(input_processor=MapCompose(remove_tags))
     post_content = Field(input_processor=MapCompose(remove_tags))
-    is_admin = Field()
-    is_special_user = Field()
+    # is_admin = Field() Uso futuro. Muito provavel terei que remodelar
+    # is_special_user = Field() Uso futuro. Muito provavel terei que remodelar
     title = Field(input_processor=MapCompose(remove_tags))
