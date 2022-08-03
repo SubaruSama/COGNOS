9d42004e8c24ba125e0a3c121d60e669e410cda7
SubaruSama <andresfauque@gmail.com>
2022-08-03 13:16:13 -0300

    Adicionando ideias e blueprint do que implementar

--- spiders/cognos/spiders/sinister_ly_spider.py
+++ spiders/cognos/spiders/sinister_ly_spider.py
@@ -9,19 +9,26 @@ class SinisterLySpiderSpider(scrapy.Spider):
         'LOG_LEVEL': 'INFO'
     }
 
-    # Preciso estar logado para procurar as coisas
+    # Preciso estar logado para procurar as coisas; nome do cookie: mybbuser
     # Gerar o token de sessão e passar por aqui; como? ler no código do onionjuicer
+    # 1
     def login(self):
         pass
 
+    # 3
     def make_search(self):
         keywords = ['vulnerability', 'exploit']
         # Juntar start_urls com keyowrds
+        # Seguir quando tiver link para a próxima página
+        # follow_link = CSS path que contém o link para a próxima página
+        # if follow_link is not None:
+        #     chamada recursiva self.make_search
         pass
 
-    # 1
+    # 2
     def start_requests(self):
-        yield scrapy.Request(url=self.start_urls[0], callback=self.extract_URL_from_forumdisplay)
+        yield scrapy.Request(url=self.start_urls[0], callback=self.make_search)
 
+    # 4
     def parse(self, response):
         pass
