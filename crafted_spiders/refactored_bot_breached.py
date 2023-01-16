1fccda03b877c1503f49ad7e6f9c0617a13f75dc
SubaruSama <andresfauque@gmail.com>
2023-01-16 09:00:29 -0500

    Working on

--- crafted_spiders/refactored_bot_breached.py
+++ crafted_spiders/refactored_bot_breached.py
@@ -1,3 +1,5 @@
+#!/home/user/Documents/COGNOS/venv_cognos/bin/python3.10
+
 import json
 import logging
 import os
@@ -32,6 +34,8 @@ urls = {
 
 
 class BreachedSpider:
+    scraped_paths = []
+
     def __init__(
         self,
         filename: str,
@@ -43,9 +47,9 @@ class BreachedSpider:
         self.filename = filename
         self.filepath = filepath
         self.file_extension = file_extension
-        self.logger = self.get_logger()
+        self.logger = self.setup_logging()
 
-    def setup_logging(self) -> None:
+    def setup_logging(self) -> logging.Logger:
         logger = logging.getLogger("selenium_log")
         logger.setLevel(logging.DEBUG)
         selenium_logger_handler = logging.FileHandler("selenium_log.log")
@@ -53,24 +57,26 @@ class BreachedSpider:
         selenium_logger_handler.setFormatter(selenium_formatter)
         logger.addHandler(selenium_logger_handler)
 
-    def get_logger(self):
-        return self.setup_logging().logger
+        return logger
+
+    # def get_logger(self):
+    #     return self.setup_logging().logger
 
     def setup_browser(self) -> WebDriver:
         try:
             self.logger.debug("Opening tor browser...")
             tor_process = launch_tbb_tor_with_stem(tbb_path=self.tbb_dir)
-            browser = TorBrowserDriver(self.tbb_dir, tor_cfg=cm.USEM_STEM)
+            browser = TorBrowserDriver(self.tbb_dir, tor_cfg=cm.USE_STEM)
 
             return browser
         except OSError as e:
             self.logger.debug(f"Something went wrong: {e}")
 
-    def get_browser(self) -> WebDriver:
-        # 1 setup_browser
-        # self.setup_browser()
-        # 2 self
-        return self.setup_browser().browser
+    # def get_browser(self) -> WebDriver:
+    #     # 1 setup_browser
+    #     # self.setup_browser()
+    #     # 2 self
+    #     return self.setup_browser().browser
 
     def close_browser(self) -> None:
         self.browser.quit()
@@ -133,21 +139,23 @@ class BreachedSpider:
         search_field.send_keys(keyword)
         search_button.click()
 
-    def get_path_all_threads(self, browser: WebDriver, html_content: str) -> list:
+    def get_path_all_threads(self, browser: WebDriver) -> list:
         # Get the path of ALL threads from ALL results in the results page
         css_thread_post_pattern = "tr.inline_row:nth-child(n) > td:nth-child(n) > div:nth-child(n) > span:nth-child(n) > a:nth-child(even)"
         css_elements = browser.find_elements(By.CSS_SELECTOR, css_thread_post_pattern)
         paths = [elem.get_attribute("href") for elem in css_elements]
+        self.scraped_paths.append(paths)
 
         # Check for next page
-        next_page_exists = self.next_page_present(browser, html_content)
+        next_page_exists = self.next_page_present(browser)
 
         if next_page_exists:
             self.go_to_next_page(browser)
-            self.get_path_all_threads(browser, html_content)
+            self.get_path_all_threads(browser)
         else:
             # Return the list of all paths
-            return paths
+            # return paths
+            return self.scraped_paths
 
     def extract_contents_from_posts(self, html_content: str) -> str:
         pass
@@ -156,17 +164,34 @@ class BreachedSpider:
         self.go_to_page(url)
         pass
 
-    def next_page_present(self, browser: WebDriver, html_content: str) -> bool:
-        pass
+    def next_page_present(self, browser: WebDriver) -> bool:
+        xpath_next_page_pattern = '/html//a[@class = "pagination_next"]'
+        try:
+            next_page_button_present = WebDriverWait(browser, 10).until(
+                EC.presence_of_all_elements_located(
+                    (By.XPATH, xpath_next_page_pattern)
+                )
+            )
+        except NoSuchElementException as e:
+            self.logger.debug(f"Exception ocurred at {self.next_page_present.__qualname__}!")
+            self.logger.debug(f"Message: {e.msg}")
+            self.logger.debug(f"Stacktrace: {e.stacktrace}")
+
+        if next_page_button_present is not None:
+            return True
+
+        return False
 
     def go_to_next_page(self, browser: WebDriver) -> None:
-        pass
+        next_page_button = browser.find_element(By.CLASS_NAME, "pagination_next")
+        next_page_button.click()
 
     @classmethod
     def write_to_file(cls, filename, filepath, file_extension) -> None:
         pass
 
     def scrape(self):
+        self.logger.debug("Logger created.")
         # 1. Gerar uma lista com todos os resultados das páginas de buscas,
         #   ir passando para a próxima pagina apenas coletando os paths das threads,
         #   onde irei salvar numa lista e irei passar path a path, sem precisar
@@ -180,58 +205,60 @@ class BreachedSpider:
         # 3. Checar se a página de busca existe próxima pagina
         #   3.1 Se sim, vai para a proxima pagina e repete o processo
         #   3.2 Se não, termina
-        logger = self.get_logger()
-        logger.debug("Logger created.")
+        try:
+            self.logger.debug("Opening browser...")
+            browser = self.setup_browser()
+            self.logger.debug("Brownser opened.")
 
-        logger.debug("Opening browser...")
-        browser = self.get_browser()
-        logger.debug("Brownser opened.")
+            self.logger.debug(f'Going to the page {urls.get("breached_login_hidden_service", "URL not registered in the dict")}')
+            self.go_to_page(browser, urls.get("breached_login_hidden_service", "URL not registered in the dict"))
+            self.logger.debug("Loading credentials...")
+            username, password = self.load_credentials(CREDENTIALS_PATH)
+            self.login(
+                username, password, browser
+            )
 
-        self.logger.debug(f'Going to the page {urls.get("breached_login_hidden_service")}')
-        self.go_to_page(urls.get("breached_login_hidden_service"))
-        self.logger.debug("Loading credentials...")
-        username, password = self.load_credentials(CREDENTIALS_PATH)
-        self.login(
-            username, password, browser
-        )
+            self.make_search(browser)
 
-        self.make_search(browser)
+            # Collect all the paths from all threads
+            html_content = self.get_page_source(browser)
+            paths = self.get_path_all_threads(browser, html_content)
+            all_paths_gathered = False
+            while all_paths_gathered is not True:
+                for path in paths:
+                    # 1. Collect the path from each thread
+                    # 2. Check if next page exists
+                    next_page_present = self.next_page_present(browser)
+                    if next_page_present:
+                        self.go_to_next_page()
+                    #   2.1 If yes, go to the next page
+                    #   2.2 If no, start scraping the threads
+                    pass
+                else:
+                    # Scrape the posts, call the method for it
+                    all_paths_gathered = True
 
-        # Collect all the paths from all threads
-        paths = self.get_path_all_threads()
-        all_paths_gathered = False
-        while all_paths_gathered is not True:
-            for path in paths:
-                # 1. Collect the path from each thread
-                # 2. Check if next page exists
-                next_page_present = self.next_page_present()
-                if next_page_present:
-                    self.go_to_next_page()
-                #   2.1 If yes, go to the next page
-                #   2.2 If no, start scraping the threads
-                pass
-            else:
-                # Scrape the posts, call the method for it
-                all_paths_gathered = True
+            # With the list of all threads, enter each thread and collect
+            # the posts
+            is_scraping_done = False
+            urls_from_threads = paths
+            while is_scraping_done is not True:
+                for url in urls_from_threads:
+                    self.enter_thread(url)
+                    post_contents = self.extract_contents_from_posts(html_content)
+                    BreachedSpider.write_to_file(post_contents)
+                    next_page_exists = self.next_page_present()
+                    if next_page_exists:
+                        self.go_to_next_page()
+                        continue
+                else:
+                    is_scraping_done = True
 
-        # With the list of all threads, enter each thread and collect
-        # the posts
-        is_scraping_done = False
-        urls_from_threads = paths
-        while is_scraping_done is not True:
-            for url in urls_from_threads:
-                self.enter_thread(url)
-                post_contents = self.extract_contents_from_posts(html_content)
-                BreachedSpider.write_to_file(post_contents)
-                next_page_exists = self.next_page_present()
-                if next_page_exists:
-                    self.go_to_next_page()
-                    continue
-            else:
-                is_scraping_done = True
+            self.close_browser()
 
-        self.close_browser()
-
+        except Exception as e:
+            self.logger.debug(f"Something went wrong at {self.scrape.__qualname__}!")
+            self.logger.debug(f"Exception: {e}")
 
 spider = BreachedSpider("scraped_results", "../../")
 spider.scrape()
